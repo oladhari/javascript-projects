d6549b95d830384587973c18f7acd4a91996a2cd
oussama ladhari <oussama.ladhari@gmail.com>
2018-07-27 18:19:41 +0900

    testing this for functions and objects

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-alert('this time external link');
+var john = {
+  fullName: "John Smith",
+  mass: 82,
+  height: 1.82,
+  calcBMI: function() {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  }
+};
+
+var neo = {
+  fullName: "Neo Bassas",
+  mass: 72,
+  height: 1.82,
+  calcBMI: function() {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  }
+};
+
+john.calcBMI();
+console.log(john.bmi);
+neo.calcBMI();
+console.log(neo.bmi);
